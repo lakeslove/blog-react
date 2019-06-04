0d0702f2ed32f29b807e4546cea68a2d49a3cdf6
liuxin <liuxin@i-counting.cn>
2019-06-04 13:37:55 +0800

    把springMVC配置改为java配置

--- src/main/java/com/lakeslove/blog/BlogApplication.java
+++ src/main/java/com/lakeslove/blog/BlogApplication.java
@@ -1,25 +1,13 @@
 package com.lakeslove.blog;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@MapperScan("com.lakeslove.blog.dao")
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
-	}
-
-	@RequestMapping("/boot.htm")
-	public String test(){
-		return "blog可以访问";
 	}
 
 }
--- src/main/java/com/lakeslove/blog/config/BaseConfig.java
+++ src/main/java/com/lakeslove/blog/config/BaseConfig.java
@@ -2,12 +2,14 @@ package com.lakeslove.blog.config;
 
 import com.lakeslove.blog.filter.LoginFilter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.AbstractServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -17,50 +19,60 @@ import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 @Configuration
 @ImportResource(
     {
-        "classpath:xml/spring_context.xml",
-        "classpath:xml/springmvc_context.xml"
+        "classpath:xml/spring_context.xml"
     }
 )
 public class BaseConfig {
 
-//    @Bean
-//    public InternalResourceViewResolver viewResolver(){
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setPrefix("/templates/views");
-//        viewResolver.setSuffix(".jsp");
-//        viewResolver.setViewClass(JstlView.class);
-//        return viewResolver;
-//    }
+  /**
+   * 配置tomcat的根目录，
+   * 因为tomcat默认的跟目录是src下面的webapp/WEB-INF
+   * @return
+   */
+  @Bean
+  public AbstractServletWebServerFactory embeddedServletContainerFactory() throws FileNotFoundException {
+    TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+    //ResourceUtils.getFile("classpath:")获取的是classes的目录
+    File resourcesFile = ResourceUtils.getFile("classpath:xml").getParentFile();
+    tomcatServletWebServerFactory.setDocumentRoot(resourcesFile);
+    return tomcatServletWebServerFactory;
+  }
 
-//    @Bean
-//    public ViewResolver viewResolver() {
-//        return new TilesViewResolver();
-//    }
-//    @Bean
-//    public TilesConfigurer tilesConfigurer(){
-//        TilesConfigurer tiles = new TilesConfigurer();
-//        tiles.setDefinitions("classpath:xml/tiles.xml");//这里的路径可以是固定路径名，也可以时模糊匹配
-//        tiles.setCheckRefresh(true);
-//        return tiles;
-//    }
+  @Bean
+  public FilterRegistrationBean loginFilter() {
+    FilterRegistrationBean<LoginFilter> registration = new FilterRegistrationBean<>();
+    registration.setName("loginFilter");
+    registration.setFilter(new LoginFilter());
+    registration.addUrlPatterns("/manager/*");
+    registration.addInitParameter("indexPath", "/login.htm");
+    registration.setOrder(1);
+    return registration;
+  }
 
-    @Bean
-    public AbstractServletWebServerFactory embeddedServletContainerFactory() {
 
-        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        tomcatServletWebServerFactory.setDocumentRoot(
-            new File("/Users/lakeslove/workspace/workspace2/blogtest/springboot/blog/src/main/resources"));
-        return  tomcatServletWebServerFactory;
-    }
+  @Bean
+  public InternalResourceViewResolver internalResourceViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/templates/views");
+    viewResolver.setSuffix(".jsp");
+    viewResolver.setViewClass(JstlView.class);
+    return viewResolver;
+  }
 
-    @Bean
-    public FilterRegistrationBean loginFilter() {
-        FilterRegistrationBean<LoginFilter> registration = new FilterRegistrationBean<>();
-        registration.setName("loginFilter");
-        registration.setFilter(new LoginFilter());
-        registration.addUrlPatterns("/manager/*");
-        registration.addInitParameter("indexPath", "/login.htm");
-        registration.setOrder(1);
-        return registration;
-    }
+  //tiles 配置开始
+  @Bean
+  public ViewResolver viewResolver() {
+    TilesViewResolver tilesViewResolver = new TilesViewResolver();
+    tilesViewResolver.setOrder(1);
+    return tilesViewResolver;
+  }
+
+  @Bean
+  TilesConfigurer tilesConfigurer() {
+    TilesConfigurer tilesConfigurer = new TilesConfigurer();
+    tilesConfigurer.setDefinitions("classpath:templates/tiles/tiles.xml");
+    tilesConfigurer.setCheckRefresh(true);
+    return tilesConfigurer;
+  }
+
 }
